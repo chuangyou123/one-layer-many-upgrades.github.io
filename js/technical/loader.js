9416c21c6798e95cebbaa97f4d95ede1ff22468f
Winster <418501074@qq.com>
2026-08-03 20:44:19 +0800

    Translate via WebTranslator

---
+++ js/technical/loader.js
@@ -0,0 +1,8 @@
+// 加载文件
+
+for (file in modInfo.modFiles) {
+    let script = document.createElement("script");
+    script.setAttribute("src", "js/" + modInfo.modFiles[file]);
+    script.setAttribute("async", "false");
+    document.head.insertBefore(script, document.getElementById("temp"));
+}
